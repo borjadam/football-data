80d40efa2dc47ecc6f96cc703e48c07cd62b822f
borjadam <70194598+borjadam@users.noreply.github.com>
2024-03-15 09:37:13 +0100

    Implement function for fc_barcelona matches

--- src/rest_api/fc_barcelona_matches.py
+++ src/rest_api/fc_barcelona_matches.py
@@ -12,7 +12,8 @@ if api_key:
 else:
     print("API key is not set.")
 
-competition_id = 2014
+competition_id = '2014'
+fc_barcelona_id = '81'
 
 def list_teams_in_competition(api_key, competition_id):
     url = f"https://api.football-data.org/v4/competitions/{competition_id}/teams"
@@ -24,7 +25,28 @@ def list_teams_in_competition(api_key, competition_id):
     else:
         print('Failed to fetch teams')
 
+#list_teams_in_competition(api_key, competition_id)
 
-list_teams_in_competition(api_key, competition_id)
 
-# ID: 81, Name: FC Barcelona
+def upcoming_matches_fc_barcelona(api_key):
+    base_url = 'https://api.football-data.org/v4/'
+    team_id = '81'
+    headers = {'X-Auth-Token': api_key}
+
+    # construct the URL to fetch matches
+    matches_url = f"{base_url}/teams/{team_id}/matches?status=SCHEDULED"
+    response = requests.get(matches_url, headers=headers)
+    if response.status_code == 200:
+        matches_data = response.json()
+        print(f"Upcoming matches for F.C Barcelona:")
+        for match in matches_data.get('matches', []):
+            competition = match['competition']['name']
+            date = match['utcDate']
+            home_team = match['homeTeam']['name']
+            away_team = match['awayTeam']['name']
+            print(f"{competition}: {home_team} vs {away_team} on {date}")
+        else:
+            print(f"failed to fecth matches. Status Code: {response.status_code}")
+
+if __name__ == "__main__":
+    upcoming_matches_fc_barcelona(api_key)
