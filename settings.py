910d9b1bd376f5948b50b02f1c12a8dbd936b904
borjadam <70194598+borjadam@users.noreply.github.com>
2024-03-21 21:01:18 +0100

    Include base_url in settings.py (DRY)

--- settings.py
+++ settings.py
@@ -5,5 +5,6 @@ from dotenv import load_dotenv
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
-SECRET_KEY = os.environ.get("API_KEY")
+API_KEY = os.environ.get("API_KEY")
+BASE_URL = "https://api.football-data.org/v4/"
 #DATABASE_PASSWORD = os.environ.get("DATABASE_PASSWORD")
